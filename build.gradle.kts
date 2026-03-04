ab0a543ad13ef06326d4221f7ec23c89bdcf162a
Hritik Bansal <hritikbansal@Hritiks-MacBook-Air.local>
2026-03-04 18:10:47 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "com.terminalbuffer"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+        showStandardStreams = true
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
